54d50bc066717a26412ffea7ac576aaf60fa0023
sapkotanavin21 <sapkotanavin21@gmail.com>
2020-08-23 19:06:34 +0545

    added some carousel view

--- app/src/main/java/com/example/pizzadelivery/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/pizzadelivery/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,23 +15,32 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.pizzadelivery.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private int[] mImages = new int[]{
+            R.drawable.a1, R.drawable.ad3, R.drawable.a1
+    };
 
+
+    CarouselView carouselView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        carouselView = view.findViewById(R.id.caral);
+        carouselView.setPageCount(mImages.length);
+        carouselView.setImageListener(new ImageListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(mImages[position]);
             }
         });
-        return root;
+        return view;
     }
 }
