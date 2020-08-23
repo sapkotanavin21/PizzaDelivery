00691166de45068ebba0471f6511fbc2f26603a2
sapkotanavin21 <sapkotanavin21@gmail.com>
2020-08-23 18:11:55 +0545

    notification added in login system

--- app/src/main/java/com/example/pizzadelivery/LoginActivity.java
+++ app/src/main/java/com/example/pizzadelivery/LoginActivity.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 
@@ -54,7 +55,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent =new Intent(LoginActivity.this,MainActivity.class);
-               // login();
+               login();
                 if (TextUtils.isEmpty(etUsername.getText())) {
                     etUsername.setError("please enter user name");
                     Vibrator vibrator=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
@@ -104,74 +105,74 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-//    private void login() {
-//        notificationManagerCompat= NotificationManagerCompat.from(this);
-//        CreateChannel channel= new CreateChannel(this);
-//        channel.createChannel();
-//         final String username =etUsername .getText().toString();
-//        String password = etPassword.getText().toString();
-//
-//        LoginBLL loginBLL = new LoginBLL();
-//        com.apsara.pizzadelivery.model.username Username=new username(username,password);
-////
-//        UserAPI usersAPI = Url.getInstance().create(UserAPI.class);
-//        Call<SignUpResponse> usersCall = usersAPI.checklogin(Username);
-//        StrictModeClass.StrictMode();
-//        try {
-//            Response<SignUpResponse> loginResponse = usersCall.execute();
-//            if (loginResponse.isSuccessful() &&
-//                    loginResponse.body().getStatus().equals("Login success!")) {
-//                SharedPreferences sharedPreferences = getSharedPreferences("Pizza",MODE_PRIVATE);
-//                SharedPreferences.Editor editor = sharedPreferences.edit();
-//                editor.putString("token","Bearer " + loginResponse.body().getToken());
-//                editor.commit();
-//
-//                Url.token += loginResponse.body().getToken();
-//                Toast.makeText(LoginActivity.this,"Login Sucessfull", Toast.LENGTH_SHORT).show();
-//                Notification notification= new NotificationCompat.Builder(LoginActivity.this, CreateChannel.CHANNEL_1).
-//                        setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
-//                        .setContentTitle("Login")
-//                        .setContentText("You are login successfully!!")
-//                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-//                        .build();
-//                notificationManagerCompat.notify(1, notification);
-//                Intent intent=new Intent(LoginActivity.this,Main2Activity.class);
-//                intent.putExtra("Userlogin",databaseList());
-//                startActivity(intent);
-//
-//                finish();
-//
-//            }
-//            else {
-//
-//                Toast.makeText(this, "Username or password is incorrect", Toast.LENGTH_SHORT).show();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//
+    private void login() {
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
+        final String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
 
-//    }
+        LoginBLL loginBLL = new LoginBLL();
+        com.apsara.pizzadelivery.model.username Username = new username(username, password);
+//
+        UserAPI usersAPI = Url.getInstance().create(UserAPI.class);
+        Call<SignUpResponse> usersCall = usersAPI.checklogin(Username);
+        StrictModeClass.StrictMode();
+        try {
+            Response<SignUpResponse> loginResponse = usersCall.execute();
+            if (loginResponse.isSuccessful() &&
+                    loginResponse.body().getStatus().equals("Login success!")) {
+                SharedPreferences sharedPreferences = getSharedPreferences("Pizza", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("token", "Bearer " + loginResponse.body().getToken());
+                editor.commit();
 
+                Url.token += loginResponse.body().getToken();
+                Toast.makeText(LoginActivity.this, "Login Sucessfull", Toast.LENGTH_SHORT).show();
+                Notification notification = new NotificationCompat.Builder(LoginActivity.this, CreateChannel.CHANNEL_1).
+                        setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
+                        .setContentTitle("Login")
+                        .setContentText("You are login successfully!!")
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .build();
+                notificationManagerCompat.notify(1, notification);
+                Intent intent = new Intent(LoginActivity.this, Main2Activity.class);
+                intent.putExtra("Userlogin", databaseList());
+                startActivity(intent);
 
-//        if(loginBLL.checkUser(username,password)){
-//             Notification notification= new NotificationCompat.Builder(LoginActivity.this, CreateChannel.CHANNEL_1).
-//                        setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
-//                        .setContentTitle("Login")
-//                        .setContentText("You are login successfully!!")
-//                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-//                        .build();
-//                notificationManagerCompat.notify(1, notification);
-//                Toast.makeText(LoginActivity.this,"Login Sucessfull",Toast.LENGTH_SHORT).show();
-//                Intent intent=new Intent(LoginActivity.this,Main2Activity.class);
-//                intent.putExtra("Userlogin",databaseList());
-//                startActivity(intent);
-//                finish();
-//                }else{
-//            Toast.makeText(this, "Either username or password is incorrect", Toast.LENGTH_SHORT).show();
-//            etUsername.requestFocus();
-//        }
+                finish();
+
+            } else {
+
+                Toast.makeText(this, "Username or password is incorrect", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
 
         }
+
+
+        if (loginBLL.checkUser(username, password)) {
+            Notification notification = new NotificationCompat.Builder(LoginActivity.this, CreateChannel.CHANNEL_1).
+                    setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
+                    .setContentTitle("Login")
+                    .setContentText("You are login successfully!!")
+                    .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                    .build();
+            notificationManagerCompat.notify(1, notification);
+            Toast.makeText(LoginActivity.this, "Login Sucessfull", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtra("Userlogin", databaseList());
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Either username or password is incorrect", Toast.LENGTH_SHORT).show();
+            etUsername.requestFocus();
+        }
+    }
+
+}
 
 
 
