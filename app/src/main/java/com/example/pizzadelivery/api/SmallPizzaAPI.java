f5940dbcbf8e153e1be256c20f098f89032ecdf7
sapkotanavin21 <sapkotanavin21@gmail.com>
2020-08-23 19:23:12 +0545

    API create for small size pizzaa

---
+++ app/src/main/java/com/example/pizzadelivery/api/SmallPizzaAPI.java
@@ -0,0 +1,26 @@
+package com.example.pizzadelivery.api;
+
+
+import com.example.pizzadelivery.model.Small;
+
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface SmallPizzaAPI {
+    @GET("smallpizza")
+    Call<List<Small>> getchickenpizza();
+    @Multipart
+    @POST("upload")
+    Call<Small> uploadImage(@Part MultipartBody.Part img);
+    @GET("smallpizza")
+    Call<Small> getImage(@Header("Authorization") String id);
+}
+
+
