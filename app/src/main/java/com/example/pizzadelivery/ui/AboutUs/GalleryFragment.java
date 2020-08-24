ffe879a54a50ed29c58c24e00c73844325932e76
sapkotanavin21 <sapkotanavin21@gmail.com>
2020-08-24 21:07:09 +0545

    about us added v1.1

--- app/src/main/java/com/example/pizzadelivery/ui/AboutUs/GalleryFragment.java
+++ app/src/main/java/com/example/pizzadelivery/ui/AboutUs/GalleryFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,19 +19,17 @@ import com.example.pizzadelivery.R;
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    WebView webview;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        webview=root.findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://www.dominos.de");
         return root;
     }
 }
